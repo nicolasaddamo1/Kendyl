5abdbaee0fedea0a5732767128df57e1e2b46c4d
Nico <nicolasaddamo1@gmail.com>
2025-08-07 12:23:22 -0300

    feat: added all needed modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { InterviewsModule } from './interviews/interviews.module';
+import { RtcModule } from './rtc/rtc.module';
+import { AiModule } from './ai/ai.module';
+import { ReportsModule } from './reports/reports.module';
+import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, InterviewsModule, RtcModule, AiModule, ReportsModule, DataModule],
   controllers: [AppController],
   providers: [AppService],
 })
